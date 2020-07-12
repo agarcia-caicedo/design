e4ed2a0c95c76f7ea1de55c488c8e82954fc766b
Ana Garcia <agarcia-caicedo@myseneca.ca>
2020-07-12 16:52:22 -0400

    fixed routing

--- design/src/index.js
+++ design/src/index.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Work from './pages/Work';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Redirect, BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Work from "./pages/Work";
+import * as serviceWorker from "./serviceWorker";
 
 const routing = (
   <Router>
     <div>
-      <Route exact path="/" component={App} />
+      <Route exact path="/design" component={App} />
+      <Route exact path="/">
+        <Redirect to="/design" />
+      </Route>
       <Route path="/About" component={About} />
       <Route path="/Contact" component={Contact} />
       <Route path="/Work" component={Work} />
     </div>
   </Router>
-)
+);
 
-ReactDOM.render(routing, document.getElementById('root'))
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
